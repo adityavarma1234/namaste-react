eff53a8d35f6833067418113df06776f930f9331
Aditya Emmadishetty <aditya@decent.com>
2025-04-24 11:11:38 +0530

    Remove comments make file clean

--- App.js
+++ App.js
@@ -1,18 +1,3 @@
-/**
- * 
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm h1 tag </h1>
- *          <h2>I'm h2 tag </h2>
- *      </div>
- * 
- * <div id="child2">
- *          <h1>I'm h1 tag </h1>
- *          <h2>I'm h2 tag </h2>
- *      </div>
- * </div>
- */
-
 const parent = React.createElement(
     "div",
     { id: "parent" },
@@ -28,23 +13,7 @@ const parent = React.createElement(
     )]
 );
 
-const paragraph = React.createElement("p", {}, "paragraph in react");
-
-
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading", xyz: "abc" },
-//     "Hello World from React!"
-// );
-
-// console.log(heading);
-console.log(parent);
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(root);
 
-// root.render(heading);
-// root.render(paragraph);
 root.render(parent);
