015c13235a7ca7ebbb002f39471de828ddbff176
Aditya Emmadishetty <aditya@decent.com>
2025-04-27 17:10:22 +0530

    Add react hook for filtering restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,70 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([
+    {
+      info: {
+        id: "152417",
+        name: "Burger King",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/24/166c48f6-5875-4509-b218-f3af425f96a0_152417.jpg",
+        avgRating: "4.3",
+        cuisines: ["Burgers", "American"],
+        sla: {
+          slaString: "45-50 mins",
+        },
+      },
+    },
+    {
+      info: {
+        id: "155266",
+        name: "Pizza Hut",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/e1a77e07-4e9b-4b5c-8c11-2debe231c746_155266.jpg",
+        avgRating: "3.9",
+        cuisines: ["Pizzas"],
+        sla: {
+          slaString: "45-50 mins",
+        },
+      },
+    },
+    {
+      info: {
+        id: "155265",
+        name: "Mehfil",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/e1a77e07-4e9b-4b5c-8c11-2debe231c746_155266.jpg",
+        avgRating: "4.2",
+        cuisines: ["Biryanis"],
+        sla: {
+          slaString: "45-50 mins",
+        },
+      },
+    },
+  ]);
+
   return (
     <div className="body">
       <div className="search">Search</div>
 
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+            console.log(listOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className="restaurant-container">
-        {restaurantList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             restaurantData={restaurant}
