50c384bdd81247f17a2874d72ee87dd6979dc807
Aditya Emmadishetty <aditya@decent.com>
2025-04-27 17:40:13 +0530

    restaurant usestate toprated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,48 +1,10 @@
 import RestaurantCard from "./RestaurantCard";
+import restaurantList from "../utils/mockData";
+
 import { useState } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState([
-    {
-      info: {
-        id: "152417",
-        name: "Burger King",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/24/166c48f6-5875-4509-b218-f3af425f96a0_152417.jpg",
-        avgRating: "4.3",
-        cuisines: ["Burgers", "American"],
-        sla: {
-          slaString: "45-50 mins",
-        },
-      },
-    },
-    {
-      info: {
-        id: "155266",
-        name: "Pizza Hut",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/e1a77e07-4e9b-4b5c-8c11-2debe231c746_155266.jpg",
-        avgRating: "3.9",
-        cuisines: ["Pizzas"],
-        sla: {
-          slaString: "45-50 mins",
-        },
-      },
-    },
-    {
-      info: {
-        id: "155265",
-        name: "Mehfil",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/e1a77e07-4e9b-4b5c-8c11-2debe231c746_155266.jpg",
-        avgRating: "4.2",
-        cuisines: ["Biryanis"],
-        sla: {
-          slaString: "45-50 mins",
-        },
-      },
-    },
-  ]);
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
 
   return (
     <div className="body">
@@ -53,7 +15,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (restaurant) => restaurant.info.avgRating > 4
+              (restaurant) => restaurant.info.avgRating > 4.2
             );
             setListOfRestaurants(filteredList);
             console.log(listOfRestaurants);
