8ec654fe0d8c385e893060a4a53dd653254e715b
Aditya Varma <nitbaditya@gmail.com>
2025-04-28 14:21:42 +0530

    Add dynamic data

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,16 @@
+import { useRouteError } from "react-router";
+
 const Error = () => {
+  const error = useRouteError();
+  console.log(error);
   return (
     <div>
       <h1>Oops!!</h1>
       <h2>Something went wrong!!</h2>
+      <p>{error.data}</p>
+      <p>
+        {error.status}: {error.statusText}
+      </p>
     </div>
   );
 };
