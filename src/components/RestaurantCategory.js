e184a807c34eaf9fdd5a4e57281af7ca6c34315a
Aditya Varma <nitbaditya@gmail.com>
2025-05-01 13:45:55 +0530

    Add accordian

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,16 +1,25 @@
 import ItemList from "./ItemList";
+import { useState } from "react";
 
 const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
+  const handleClick = () => {
+    console.log("clicked");
+    setShowItems(!showItems);
+  };
   return (
     <div>
       <div className=" w-6/12 bg-gray-50 shadow-lg mx-auto p-4 my-4">
-        <div className="flex justify-between">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
           <span className="font-bold">
             {data.title} ({data.itemCards.length})
           </span>
           <span>⬇️</span>
         </div>
-        <ItemList items={data.itemCards} />
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
     </div>
   );
