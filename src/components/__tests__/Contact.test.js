1bda8353697d4a7e5f70e46a672e9393ebd60ed1
Aditya Varma <nitbaditya@gmail.com>
2025-05-06 14:03:08 +0530

    move test cases to a describe block

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,10 +2,26 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
+describe("Contact Us, Page Test Case", () => {
+  it("Should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
 
-  expect(heading).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("Should load button inside contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("Should include submit text ", () => {
+    render(<Contact />);
+    const button = screen.getByText("Submit");
+
+    expect(button).toBeInTheDocument();
+  });
 });
